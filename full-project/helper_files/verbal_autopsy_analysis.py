36bed6d3a09fb4cb455b7c82a13fcd58aee13812
valentina-s <val_is_everywhere@yahoo.com>
2015-09-28 23:38:08 -0700

    ProjectOrganization Lesson initial commit

---
+++ full-project/helper_files/verbal_autopsy_analysis.py
@@ -0,0 +1,39 @@
+# --------------------------------------------------
+# Load the necessary python modules
+# --------------------------------------------------
+
+
+
+
+# --------------------------------------------------
+# Load the verbal autopsy analysis module
+# You need to reload it every time you make changes
+# --------------------------------------------------
+
+
+
+
+
+# --------------------------------------------------
+# Read the data files
+# Make sure to include the correct data path
+# --------------------------------------------------
+
+
+
+
+
+
+# --------------------------------------------------
+# Iterate over each word and
+# 1) generate the data frame
+# 2) write it to a file in the results directory
+#
+# Note: Can you think of a way to automatically
+# generate the names of the file using strings. Will your
+# code work if I ask you to generate tables for
+# 100 different words?
+# --------------------------------------------------
+
+
+
---
+++ full-project/helper_files/verbal_autopsy_functions.py
@@ -0,0 +1,22 @@
+# --------------------------------------------------------------
+# Load the necessary python modules
+# --------------------------------------------------------------
+
+# --------------------------------------------------------------
+# Define a function counts_per_word
+#
+# Inputs
+# ------
+#         df - pandas data frame
+#         word - a string of the form 'word_fever'
+# Returns
+# -------
+#         counts_table - pandas data frame with
+#         columns - Site, Cause of Death, Times word is mentioned
+#
+# Note: all you need to do is to extract the necessary statements
+#       from the pandas session notebook: look at pivot tables
+#       and inverse pivot tables
+# ---------------------------------------------------------------
+
+
